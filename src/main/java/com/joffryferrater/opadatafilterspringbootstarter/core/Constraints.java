2d366f465e274024e2878b5547ae4b155ba6ad44
Joffry Ferrater <joffry.ferrater@bisnode.com>
2020-02-25 23:43:19 +0100

    AND operation constraints test

---
+++ src/main/java/com/joffryferrater/opadatafilterspringbootstarter/core/Constraints.java
@@ -0,0 +1,12 @@
+package com.joffryferrater.opadatafilterspringbootstarter.core;
+
+import com.joffryferrater.opadatafilterspringbootstarter.model.response.Predicate;
+
+import java.util.List;
+
+public class Constraints {
+
+    public String andConstraints(List<Predicate> predicates) {
+        return null;
+    }
+}
