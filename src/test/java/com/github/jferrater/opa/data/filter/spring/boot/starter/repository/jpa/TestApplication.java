708f8d128f3cc47ad267abb5416a9d9f540e90b6
Joffry Ferrater <joffry.ferrater@bisnode.com>
2020-04-22 12:56:44 +0200

    Ab#36: Decouple EntityManagerFactory

--- src/test/java/com/github/jferrater/opa/data/filter/spring/boot/starter/repository/jpa/TestApplication.java
+++ src/test/java/com/github/jferrater/opa/data/filter/spring/boot/starter/repository/jpa/TestApplication.java
@@ -3,10 +3,12 @@ package com.github.jferrater.opa.data.filter.spring.boot.starter.repository.jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.github.jferrater.opa.data.filter.spring.boot.starter",
 "com.github.jferrater.opa.ast.db.query.config"})
+@Import(MyJpaConfig.class)
 public class TestApplication {
 
     public static void main(String[] args) {
