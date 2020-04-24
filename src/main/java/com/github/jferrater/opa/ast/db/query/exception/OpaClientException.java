a8a7710e73657ded29ddc5eb7d76f2f7a30a9a2e
Joffry Ferrater <joffry.ferrater@bisnode.com>
2020-04-25 01:32:10 +0200

    Removed unwanted files

--- src/main/java/com/github/jferrater/opa/ast/db/query/exception/OpaClientException.java
+++
@@ -1,8 +0,0 @@
-package com.github.jferrater.opa.ast.db.query.exception;
-
-public class OpaClientException extends RuntimeException {
-
-    public OpaClientException(String message) {
-        super(message);
-    }
-}
--- src/test/java/com/github/jferrater/opa/data/filter/spring/boot/starter/repository/jpa/MyOpaDataFilterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.jferrater.opa.data.filter.spring.boot.starter.repository.jpa;
-
-import com.github.jferrater.opa.data.filter.spring.boot.starter.repository.jpa.entity.PetEntity;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MyOpaDataFilterRepository extends OpaDataFilterRepository<PetEntity, Long> {
-
-}
