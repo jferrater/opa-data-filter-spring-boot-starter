f661fd2425047379d78fe5f4289a59c12bc13ffd
Joffry Ferrater <joffry.ferrater@bisnode.com>
2020-04-25 13:44:20 +0200

    Ab#25: Added mongo repository configuration

--- opa-datafilter-mongo-spring-boot-starter/src/main/java/com/github/jferrater/opadatafiltermongospringbootstarter/config/MongoRepositoryConfig.java
+++ opa-datafilter-mongo-spring-boot-starter/src/main/java/com/github/jferrater/opadatafiltermongospringbootstarter/config/MongoRepositoryConfig.java
@@ -1,4 +1,15 @@
 package com.github.jferrater.opadatafiltermongospringbootstarter.config;
 
+import com.github.jferrater.opadatafiltermongospringbootstarter.repository.OpaDataFilterMongoRepositoryImpl;
+import com.github.jferrater.opadatafiltermongospringbootstarter.repository.OpaMongoRepositoryFactoryBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(
+        basePackages = {"com.github.jferrater.opadatafiltermongospringbootstarter.repository"},
+        repositoryBaseClass = OpaDataFilterMongoRepositoryImpl.class,
+        repositoryFactoryBeanClass = OpaMongoRepositoryFactoryBean.class
+)
 public class MongoRepositoryConfig {
 }
