1160666a08af80df170aae13995ba388cb2d9908
Joffry Ferrater <joffry.ferrater@bisnode.com>
2020-05-19 23:46:57 +0200

    Fixes #13: project setup for partial request query service

---
+++ opa-datafilter-query-service/src/main/java/com/github/jferrater/opa/datafilter/query/service/OpaDatafilterQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.jferrater.opa.datafilter.query.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpaDatafilterQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpaDatafilterQueryServiceApplication.class, args);
+	}
+
+}
